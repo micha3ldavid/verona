56492df3f82efb137bbc7c8fade07e68880a1301
Michael David <michaeldavid.juliano@gmail.com>
2017-02-24 01:10:35 -0700

    mobile nav

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -1,24 +1,47 @@
 
-window.addEventListener('load', function () {
+!(function () {
 
-    var btn = Mk.$('#btn-nav-mobile'),
-        menu = Mk.$('#nav-mobile'),
-        hidden;
+    function open (btn, menu, body) {
 
-    btn.on('click', function (e) {
+        btn.find('.fa').addClass('fa-close').removeClass('fa-navicon');
+        menu.attr('aria-hidden', 'false');
+        body.addClass('noflow');
+    }
 
-        e.preventDefault();
+    function close (btn, menu, body) {
 
-        hidden = menu.attr('aria-hidden');
-        menu.attr('aria-hidden', hidden === 'true' ? 'false' : 'true');
+        btn.find('.fa').addClass('fa-navicon').removeClass('fa-close');
+        menu.attr('aria-hidden', 'true');
+        body.removeClass('noflow');
+    }
+
+    window.addEventListener('load', function () {
+
+        var body = Mk.$('body'),
+            btn  = Mk.$('#btn-nav-mobile'),
+            menu = Mk.$('#nav-mobile'),
+            hidden;
+
+        btn.on('click', function (e) {
+
+            e.preventDefault();
+
+            hidden = menu.attr('aria-hidden') === 'true';
+
+            if (hidden) {
+                open(btn, menu, body);
+                return;
+            }
+            close(btn, menu, body);
+        });
+
+        btn.on('blur', function (e) {
+            close(btn, menu, body);
+        });
+
+        menu.on('mousedown', true, function (e) {
+            e.preventDefault();
+        });
     });
 
-    btn.on('blur', true, function (e) {
-
-        var el = Mk.$(e.relatedTarget);
-
-        if (el.parent('#menu-mobilenav').length < 1) {
-            menu.attr('aria-hidden', 'true');
-        }
-    });
-});
+})();
