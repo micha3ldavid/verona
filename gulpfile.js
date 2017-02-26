b26b1f44d534fec8a152edd3d3eef6c83209bfa6
Michael David <michaeldavid.juliano@gmail.com>
2017-02-26 15:52:07 -0700

    clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 
 var gulp  = require('gulp'),
+	mini  = require('gulp-minify'),
+	clean = require('gulp-clean-css'),
 	sass  = require('gulp-sass');
 	paths = {
 		'style': {
-			sass: './dist/scss/*.scss',
-			output: './dist/css'
+			sass: './src/scss/*.scss',
+			output: './src/css'
 		}
 	};
 
@@ -13,4 +15,11 @@ gulp.task('sass', function () {
 	gulp.src(paths.style.sass)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(paths.style.output));
+});
+
+gulp.task('clean', function() {
+
+  return gulp.src('./src/css/*.css')
+    .pipe(clean({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/css'));
 });
