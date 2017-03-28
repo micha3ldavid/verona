19344fba86dd95bc3b5f35f61e1d5d2a3bb17bd8
Michael David <michaeldavid.juliano@gmail.com>
2017-03-27 19:53:31 -0600

    bunch more content

--- routes/controllers/customs.js
+++ routes/controllers/customs.js
@@ -9,12 +9,13 @@ module.exports = function (express, app) {
 
     app.get('/customs', function (req, res) {
 
-        store.pull(['views', 'navigations', 'mail'], function (result) {
+        store.pull(['views', 'navigations', 'mail', 'customs'], function (result) {
 
             var data = {
                 page: result.views.customs,
                 navigations: result.navigations,
-                mailer: result.mail.transport.customs
+                mailer: result.mail.transport.customs,
+                latest: result.customs.items
             };
 
             res.render(data.page.view, data);
